d4b293292066cc06ae64a9e1f77c0c041f55075e
markbigmuscle-jpg <markbigmuscle@gmail.com>
2025-10-22 08:30:34 +0800

    Update camera.php

--- camera.php
+++ camera.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Camera</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.11.9/p5.min.js"></script>
-    <script src="https://unpkg.com/ml5@1/dist/ml5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.11.9/p5.min.js"></script> <!-- for drawing of face keypoints-->
+    <script src="https://unpkg.com/ml5@1/dist/ml5.min.js"></script> <!-- for detecting face-->
   </head>
   <body>
 	<div class="box" style="width:600px; margin:auto;">
@@ -52,4 +52,5 @@
 		</form>
 	</div>
   </body>
+
 </html>
