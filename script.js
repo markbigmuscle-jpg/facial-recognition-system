757ebf3dce0c27d5edacdcdbfa73004146a4cfd7
markbigmuscle-jpg <markbigmuscle@gmail.com>
2025-10-22 09:47:02 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var users = [];
-
-function StartRegistration() {
-	const w = 800;
-	const h = 400;
-	
-	window.open("http://localhost/face recognition/registration.php","Registration",`width=${w},height=${h},titlebar=0,top=${(screen.height-h)/2},left=${(screen.width-w)/2}`)
-}
